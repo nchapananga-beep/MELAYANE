e8a03cba23c3e65cd02fbc0b51b7a0fb023bf2af
nchapananga-beep <nchapananga@unisced.edu.mz>
2026-09-08 00:08:50 +0200

    Adicionar service worker PWA do MELAYANE

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+const MELAYANE_CACHE = "melayane-pwa-v1";
+
+const MELAYANE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./offline.html",
+  "./assets/icon-melayane-192-final.png",
+  "./assets/icon-melayane-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(MELAYANE_CACHE).then(function (cache) {
+      return cache.addAll(MELAYANE_ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (chaves) {
+      return Promise.all(
+        chaves.map(function (chave) {
+          if (chave !== MELAYANE_CACHE) {
+            return caches.delete(chave);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  var pedido = event.request;
+  var url = new URL(pedido.url);
+
+  if (pedido.method !== "GET") {
+    return;
+  }
+
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(pedido)
+      .then(function (resposta) {
+        var copia = resposta.clone();
+
+        caches.open(MELAYANE_CACHE).then(function (cache) {
+          cache.put(pedido, copia);
+        });
+
+        return resposta;
+      })
+      .catch(function () {
+        return caches.match(pedido).then(function (respostaCache) {
+          if (respostaCache) {
+            return respostaCache;
+          }
+
+          if (pedido.mode === "navigate") {
+            return caches.match("./offline.html");
+          }
+
+          return Response.error();
+        });
+      })
+  );
+});
